7b440b1546620672585d246544fc20da2e2619e2
Danillo <danillo.ooo@hotmail.com>
2021-03-31 00:11:54 -0300

    Segunda aula, comportamento do formulário

---
+++ listaDeTarefas.js
@@ -0,0 +1,11 @@
+const criarTarefa = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    const valor = input.value;
+    console.log( valor );
+    input.value = " ";
+}
+
+const novaTarefa = document.querySelector('[data-form-button]');
+
+novaTarefa.addEventListener('click', criarTarefa);
